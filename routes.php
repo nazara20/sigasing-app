03bae7050d88183f3291c653ab0798fa04cf31b1
Muhammad Nazar Abdillah <muhammad.nazar.abd20@gmail.com>
2022-01-29 22:40:55 +0800

    Menampilkan halaman read lokasi

--- routes.php
+++ routes.php
@@ -1,15 +1,19 @@
 <?php
-if (isset($_GET['page'])) {
-    $page = $_GET ['page'];
+if (isset($_REQUEST['page'])) {
+    $page = $_REQUEST['page'];
     switch ($page) {
         case '';
         case 'home';
-        file_exists('pages/home.php') ? include 'pages/home.php' : include "pages/404.php";
-        break;
+            file_exists('pages/home.php') ? include 'pages/home.php' : include "pages/404.php";
+            break;
+            case '';
+        case 'lokasiread':
+            file_exists('pages/admin/lokasiread.php') ? include
+                'pages/admin/lokasiread.php' : include "pages/404.php";
+            break;
         default:
-        include "pages/404.php";
+            include "pages/404.php";
     }
-    
-}else {
+} else {
     include "pages/home.php";
 }

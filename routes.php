cabfc0f7c467692de5fcd44049c41fc643b8bb7e
Muhammad Nazar Abdillah <muhammad.nazar.abd20@gmail.com>
2022-02-05 19:57:02 +0800

    Membuat Halaman Tambah Data

--- routes.php
+++ routes.php
@@ -6,10 +6,13 @@ if (isset($_REQUEST['page'])) {
         case 'home';
             file_exists('pages/home.php') ? include 'pages/home.php' : include "pages/404.php";
             break;
-            case '';
         case 'lokasiread':
             file_exists('pages/admin/lokasiread.php') ? include
                 'pages/admin/lokasiread.php' : include "pages/404.php";
+            break;
+        case 'lokasicreate':
+            file_exists('pages/admin/lokasicreate.php') ? include
+                'pages/admin/lokasicreate.php' : include "pages/404.php";
             break;
         default:
             include "pages/404.php";
